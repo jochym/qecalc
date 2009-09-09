a18dd0a0bd0ed1dfb47c2c2120d8be69453c489a
markovsk <markovsk@4efde5e4-ebd3-4001-8375-eeb568285340>
2009-09-09 21:46:12 +0000

    Worked on plotting phonon dispersions and QHA
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@343 4efde5e4-ebd3-4001-8375-eeb568285340

--- dispersion.py
+++ dispersion.py
@@ -17,11 +17,46 @@ hexSymPoints = {
             'A' :     [0.0, 0.0, 1.0/2.0]
 }
 
+#Simple cubic symmetry points:
+SCubSymPoints = {
+            'Gamma' : [0.0, 0.0, 0.0],
+            'Gamma2': [1.0, 1.0, 1.0],
+            'X'     : [0.5, 0.0, 0.0],
+            'M'     : [0.5, 0.5, 0.0],
+            'R'     : [0.5, 0.5, 0.5]
+}
+
+#Body Centered cubic symmetry points:
+BCCubSymPoints = {
+            'Gamma' : [0.0, 0.0, 0.0],
+            'Gamma2': [1.0, 1.0, 1.0],
+            'H'     : [0.5, 0.5, -0.5],
+            'P'     : [0.25, 0.25, 0.25],
+            'N'     : [0.0, 0.0, 0.5]
+}
+
+
+#Face Centered cubic symmetry points:
+FCCubSymPoints = {
+            'Gamma' : [0.0, 0.0, 0.0],
+            'Gamma2': [1.0, 1.0, 1.0],
+            'X'     : [0.5, 0.5, 0.0],
+            'L'     : [0.5, 0.5, 0.5],
+            'W'     : [0.5, 0.75, 0.25]
+}
+
+
+
+
 class Dispersion():
     def __init__(self, qe):
         self.__points = []
+        self.__dispersion = []
         self.__path = []
         self.__qe = qe
+
+    def setValues(self, array):
+        self.__dispersion = array
 
     def setPath(self, *pathNPoints):
         import copy
@@ -59,7 +94,6 @@ class Dispersion():
             for i, ipnt in enumerate(nPoints):
                 numPoints.append(ipnt + numPoints[i])
             for symbol, ipnt in zip(path, numPoints ):
-                print hexSymPoints[symbol]
                 kPoints.append(hexSymPoints[symbol])
                 self.__points.append((symbol, ipnt))
         else:
@@ -78,8 +112,7 @@ class Dispersion():
             point1 = kPoints[iPoint + 1]
             dPoint = [ float(point1[i] - point[i])/nPoints[iPoint] \
                 for i in range(len(point)) ]
-            dPoint.append( min_gt( ( abs( dPoint[i] )  \
-                           for i in range(len(dPoint) ) ), 0) )
+            dPoint.append( min_gt( [abs(dPoint[i]) for i in range(len(dPoint))], 1.0e-7) ) # 1.0e-7 to deal with ~ 0.0 points, like 1.e-17
             lastElem = len(Path) - 1
             for i in range(nPoints[iPoint]):
                 Path.append( addPoints( Path[lastElem + i], dPoint ) )
@@ -92,46 +125,48 @@ class Dispersion():
                     elem[i] = 0.0e0
             self.__path.append(elem[0:3])
             self.__axis.append(elem[3])
-            format = "%1.12f  %1.12f  %1.12f  %1.5f"
-            print format % (elem[0], elem[1], elem[2], elem[3])
+#            format = "%1.12f  %1.12f  %1.12f  %1.5f"
+#            print format % (elem[0], elem[1], elem[2], elem[3])
 
-        print "Number of phonons in list: ", sum(nPoints) + 1
+#        print "Number of phonons in list: ", sum(nPoints) + 1
 
 
     def printPoints(self):
         for elem in self.__points:
             print elem[0], elem[1]
 
-    def matdyn(self, *pathNPoints):
+    def setPhononPath(self, *pathNPoints):
         from parser.qe_io_dict import *
         matdynIn = read_file(self.__qe.matdynInput)
         keyStart = find_key_from_string(matdynIn, '/')
         newDic = {}
-        for i in range(keyStart+1):
+        for i in range(keyStart+1)[1:]:
             newDic[i] = matdynIn[i]
         save_dic(newDic, self.__qe.matdynInput)
-#        ikey = keyStart + 1
-#        print matdynIn
-#        while ikey < len(matdynIn):
-#            print ikey
-#            print matdynIn[ikey]
-#            del matdynIn[ikey]
-#        save_dic(matdynIn, self.__qe.matdynInput)
 
         self.setPath(*pathNPoints)
 
         file = open(self.__qe.matdynInput, 'a')
         file.write(self.__toMatdynString())
+        file.close()
         self.__qe.matdynLauncher()
-        pol, omega, qpoints =  self.getMultiPhonon()
-        return omega
+        pol, self.__dispersion, qpoints =  self.__qe.getMultiPhonon()
 
 
-    def plot(self, valPath):
+    def plot(self):
         """Takes numpy array of values (nqpoints, nbands/nmodes)"""
         import pylab
-        for i in range(valPath.shape[1]):
-            pylab.plot(self.__axis, self.valPath[:,i])
+        import matplotlib
+        legStr = []
+        markerStyle = ['b.', 'g.', 'r.', 'c.', 'm.', 'y.', 'k.', 'b,', 'g,', 'r,', 'c,', 'm,', 'y,', 'k,']
+#        cmap = matplotlib.colors.ListedColormap(colors, name = 'myPlotCollors', N = self.__dispersion.shape[1])
+        for i in range(self.__dispersion.shape[1]):
+            pylab.plot(self.__axis, self.__dispersion[:,i], markerStyle[i])#, color = colors[i])
+
+            legStr.append(str(i))
+
+#            pylab.ylim([0.0, 3.0])
+        pylab.legend(legStr, numpoints = 1)
         for i in range(len(self.__points)):
             xcoord = self.__axis[ self.__points[i][1] ]
             pylab.axvline(x=xcoord, color='black')
@@ -141,9 +176,8 @@ class Dispersion():
     def __toMatdynString(self):
         string = str(len(self.__path)) + '\n'
         for elem, coord in zip(self.__path, self.__axis):
-            for e in elem:
-                string = string + str(e) + '    '
-            string = string + str(coord) + '\n'
+            string = string + \
+                   "%f    %f    %f    %f\n" % (elem[0], elem[1], elem[2], coord)
         return string
 
 
