2e9a8aabfa623da62fe5e0c2c263c8d0c7136ded
markovsk <markovsk@4efde5e4-ebd3-4001-8375-eeb568285340>
2009-10-21 01:53:06 +0000

    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@603 4efde5e4-ebd3-4001-8375-eeb568285340

--- examples/output.matdyn/matdyn_output.py
+++ examples/output.matdyn/matdyn_output.py
@@ -4,9 +4,9 @@ from qecalc.qetask.taskmatdyn import MatdynTask
 setting = Setting('config.ini')
 matdyn = MatdynTask(setting)
 matdyn.output.parse()
-Pol, Omegas, qPoints = matdyn.output.property('multi phonon', withUnits = True)
+Pol, Omegas, qPoints = matdyn.output.property('multi phonon')
 print Pol
-print Pol[0].shape[0]
+print Omegas
 #setting.matdynModes = 'blahblah.modes'
 #matdyn.output.parse()
 #mphonOut, Omegas, qPoints = matdyn.output.property('multi phonon', withUnits = True)
