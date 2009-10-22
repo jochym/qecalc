66ef19196c6052bb548a32bed9aafb889e5ecbde
markovsk <markovsk@4efde5e4-ebd3-4001-8375-eeb568285340>
2009-10-22 22:05:26 +0000

    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@664 4efde5e4-ebd3-4001-8375-eeb568285340

--- qecalc/qetask/taskq2r.py
+++ qecalc/qetask/taskq2r.py
@@ -21,6 +21,7 @@ class Q2RTask(QETask):
     def __init__(self, setting, cleanOutDir = False):
         QETask.__init__(self, setting, cleanOutDir)
         self.input = QEInput(self.setting.q2rInput, type = 'q2r')
+        self.output = QEOutput(self.setting, type = 'pw')
         self.cmdStr = "q2r.x < " + self.setting.q2rInput + " > " + \
                        self.setting.q2rOutput
         self.name = 'q2r.x'
