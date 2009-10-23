a68348194ee41693c22509e214369e8dca895858
brandon <brandon@4efde5e4-ebd3-4001-8375-eeb568285340>
2009-10-23 00:34:20 +0000

    yet another revision of the transfer script (it's a pain cuz machine supposed to do this, but not working...prob. because cron job user is not root)...at least individual users can use it now
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@670 4efde5e4-ebd3-4001-8375-eeb568285340

--- DOC/sphinx/transfer.sh
+++ DOC/sphinx/transfer.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-ROOT_UID=0   # Root has $UID 0.
 
-if [[ $USER -eq root ]] 
-then 
-	su jbk #become someone with permission to move the docs 
-fi
 
 if [[ $USER -eq jbk ]] 
 then 
-svn up
-make html
-scp -r _build/html/* jbrkeith@login.cacr.caltech.edu:projects/danse/docs.danse.us/docroot/AbInitio/qecalc
+	svn up
+	make html
+	scp -r _build/html/* jbrkeith@login.cacr.caltech.edu:projects/danse/docs.danse.us/docroot/AbInitio/qecalc
+	# hi nikolay...just do a script like the one above...
+elif [[ $USER -eq root ]] 
+then 
+	su jbk #become someone with permission to move the docs 
 fi
-
-# hi nikolay...just do a script like the one above...
