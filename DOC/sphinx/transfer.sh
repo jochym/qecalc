86f6280b90e2fc235410ec4b3f9a2b6431d764c1
brandon <brandon@4efde5e4-ebd3-4001-8375-eeb568285340>
2009-10-24 19:22:45 +0000

    minor
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@691 4efde5e4-ebd3-4001-8375-eeb568285340

--- DOC/sphinx/transfer.sh
+++ DOC/sphinx/transfer.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-
-
-if [[ $USER -eq jbk ]] 
-then 
+if [[ $USER -eq jbk ]] then 
 	svn up
 	make html
 	scp -r _build/html/* jbrkeith@login.cacr.caltech.edu:projects/danse/docs.danse.us/docroot/AbInitio/qecalc
 	# hi nikolay...just do a script like the one above...
-elif [[ $USER -eq root ]] 
-then 
+elif [[ $USER -eq root ]] then 
 	su jbk #become someone with permission to move the docs 
 fi
