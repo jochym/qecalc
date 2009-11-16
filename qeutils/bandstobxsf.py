ec45ab54e5d030bf83fbe1b0221c3e46f7516e29
markovsk <markovsk@4efde5e4-ebd3-4001-8375-eeb568285340>
2009-11-16 20:50:56 +0000

    bands to bxsf format converter for XCrysDen seems to work now
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@920 4efde5e4-ebd3-4001-8375-eeb568285340

--- qeutils/bandstobxsf.py
+++ qeutils/bandstobxsf.py
@@ -15,13 +15,13 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
+import numpy
 
-
-def bandstobxsf(energy, kpoints, recipLatticeVectors, bands, fname):
+def bandstobxsf(energy, recipLatticeVectors, bands, fname):
 
     file = open(fname,'w')
 
-    kpoints = [bands.shape[]]
+    kpoints = [bands.shape[1],bands.shape[2],bands.shape[3]]
     s =  """ BEGIN_INFO
    #
    # this is a Band-XCRYSDEN-Structure-File
@@ -32,19 +32,21 @@ def bandstobxsf(energy, kpoints, recipLatticeVectors, bands, fname):
  BEGIN_BLOCK_BANDGRID_3D
  band_energies
  BANDGRID_3D_BANDS\n"""
-    s = s + '%5u\n'%bands.shape[1] + \
+    s = s + '%5u\n'%bands.shape[0] + \
              '%5u %5u %5u\n'%(kpoints[0], kpoints[1],kpoints[2]) + \
              '  %#.8f %#.8f %#.8f\n'%(0.0, 0.0, 0.0)
     for i in range(recipLatticeVectors.shape[0]):
         s = s + '  %#.8f %#.8f %#.8f\n'%(recipLatticeVectors[i,0], \
                             recipLatticeVectors[i,1], recipLatticeVectors[i,2])
-    for iband in range(bands.shape[1]):
+    for iband in range(bands.shape[0]):
         s = s + 'BAND:%5u\n'%(iband+1)
 
         for kz in range(kpoints[2]):
             for ky in range(kpoints[1]):
                 for kx in range(kpoints[0]):
-                    s = s + '   %#.8f %#.8f %#.8f %#.8f %#.8f\n'%(bands[])
+                    s = s + '%#.4f '%(bands[iband, kx, ky, kz])
+                s = s + '\n'
+            s = s + '\n'
 
     s = s + ' END_BANDGRID_3D\n END_BLOCK_BANDGRID_3D'
     file.write(s)
@@ -58,12 +60,25 @@ if __name__ == "__main__":
     pw.input.parse()
     pw.output.parse()
     #print pw.output.property('fermi energy')
-    print pw.output.property('bands')
+    kpoints = [17,17,17]
+    bands = numpy.zeros((pw.output.property('bands')[1].shape[1], kpoints[0], kpoints[1], kpoints[2]))
+    print bands
+    for iband in range(pw.output.property('bands')[1].shape[1]):
+        for kz in range(kpoints[2]):
+            for ky in range(kpoints[1]):
+                for kx in range(kpoints[0]):
+                    bands[iband,kx,ky,kz] = \
+                      pw.output.property('bands')[1][\
+                      kpoints[2]*kpoints[1]*kz + kpoints[1]*ky + kx,iband]
+    #bands = pw.output.property('bands')[1].transpose().reshape(\
+    #                             -1, kpoints[0],kpoints[1],kpoints[2])
+    print bands
+    print bands.shape
     #pw.output.property('fermi energy')[0]
-    bandstobxsf(7.8, [17, 17, 17], \
+    bandstobxsf(7.8071, \
                 pw.input.structure.lattice.diffpy().reciprocal().base* \
                 pw.input.structure.lattice.a, \
-                pw.output.property('bands')[1], 'test.bxsf')
+                bands, 'test.bxsf')
     print "Hello World";
 
 __author__="Nikolay Markovskiy"
