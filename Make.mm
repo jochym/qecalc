e571e94a9b3a6a37db52e1f718c5516d297be903
linjiao <linjiao@4efde5e4-ebd3-4001-8375-eeb568285340>
2010-01-20 20:59:55 +0000

    tenatively add a Make.mm
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@1080 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ Make.mm
@@ -0,0 +1,61 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2005  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+PROJECT = qecalc
+PACKAGES = eqcalc
+
+# directory structure
+
+BUILD_DIRS = \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+#--------------------------------------------------------------------------
+#
+
+all: export
+	BLD_ACTION="all" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+
+
+export:: export-python-packages
+        BLD_ACTION="export" $(MM) recurse
+
+
+PACKAGES= \
+	qecalc \
+	qeutils \
+
+RSYNC=rsync -a
+
+export-python-packages::
+	for x in $(PACKAGES); do { \
+	  $(RSYNC) $$x/ $(EXPORT_ROOT)/modules/$$x/; \
+	} done
+
+
+# version
+# $Id$
+
+# End of file
