9116d52be8b816813e4f59f4416783a188116330
markovsk <markovsk@4efde5e4-ebd3-4001-8375-eeb568285340>
2010-05-18 02:07:53 +0000

    added parser of atomic positions for cp.x. Example is in tests/tasks/cp/cpout.py
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@1193 4efde5e4-ebd3-4001-8375-eeb568285340

--- qecalc/qetask/qeparser/outputs/cp.py
+++ qecalc/qetask/qeparser/outputs/cp.py
@@ -19,8 +19,39 @@ class Output(BaseOutput):
 
     def __init__(self):
         BaseOutput.__init__(self)
-        self.parsers = {}
+        self.parsers = {
+                        'trajectory'       : self.getTrajectory,
+                        }
+    def getTrajectory(self, setting):
 
+        trajectory = {
+                       'pos': [],
+                       'vel': [],}
+
+        #read Espresso output into memory:
+        file = open(setting.get('cpOutput'))
+        cpOut = file.readlines()
+        posList =  [i for i,line in enumerate(cpOut) \
+                                    if '* Physical Quantities at step:' in line]
+        for i, iStart in enumerate(posList):
+            if i < len(posList) - 1:
+                iEnd = i+1
+            else:
+                iEnd = len(cpOut) - 1
+            for i in range(iStart, iEnd):
+                line = cpOut[i]
+                if 'ATOMIC_POSITIONS' in line:                
+                    atoms = []
+                    j = i + 1
+                    while cpOut[j].strip() != "":
+                        coord = [ float(w) for w in cpOut[j].split()[1:]]
+                        atoms.append(coord)
+                        j = j + 1
+                    trajectory['pos'].append(atoms)
+        #print cpOut
+        #print trajectory
+        return [(trajectory, None)]
+        
 
 if __name__ == "__main__":
     print "Hello World";
---
+++ tests/tasks/cp/cpout.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# QEcalc              by DANSE Inelastic group
+#                     Nikolay Markovskiy
+#                     California Institute of Technology
+#                     (C) 2010  All Rights Reserved
+#
+# File coded by:      Nikolay Markovskiy
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from qecalc.qetask.cptask import CPTask
+configString = """
+# all the relevant input files must be preconfigured for specific tasks
+# before using this class
+
+[Launcher]
+# parallelization parameters
+# if this section is empty - serial mode is used
+paraPrefix:   mpiexec -n 8
+
+outdir: temp/
+
+[cp.x]
+# pw input/output files
+cpInput:  cp.in
+cpOutput: nh3cp.out
+
+"""
+
+if __name__ == "__main__":
+    cp = CPTask(configString = configString)
+    cp.syncSetting()
+    cp.output.parse()
+    print cp.output.property('trajectory')['pos']
+
+__author__="Nikolay Markovskiy"
+__date__ ="$May 17, 2010 6:34:30 PM$"
