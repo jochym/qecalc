30bc005c54dbc385bc8848338e0e8da3c9524af1
dexity <dexity@4efde5e4-ebd3-4001-8375-eeb568285340>
2011-03-31 20:32:56 +0000

    Added dummy test, that tries to import classes from matter
    git-svn-id: svn://danse.us/AbInitio/espresso/qecalc@1323 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ tests/calcs/test_import.py
@@ -0,0 +1,22 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2011  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# Dummy test: tries to import classes from matter
+
+from matter.Structure import Structure
+from matter.Lattice import cosd, Lattice
+from matter.SymmetryUtilities import equalPositions
+
+__date__ = "$Mar 31, 2011 1:29:06 PM$"
+
+
